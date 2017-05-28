61ef926e0ad7fcf8cd7645bc79977b2410d5d32a
John Fu <johnfumaster@gmail.com>
2017-05-28 22:44:24 +1000

    Added skeleton code to App.js and ShowTile.js.  Unit tests pasing

---
+++ src/components/ShowTile/ShowTile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+const ShowTile = (props) => (
+  <div className="showTile">
+
+  </div>
+);
+
+export default ShowTile;
