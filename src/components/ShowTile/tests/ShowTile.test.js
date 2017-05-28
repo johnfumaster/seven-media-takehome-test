8b012b2ceaaa7f90d139de383258197c976da50f
John Fu <johnfumaster@gmail.com>
2017-05-29 02:18:53 +1000

    Update unit tests

--- src/components/ShowTile/tests/ShowTile.test.js
+++ src/components/ShowTile/tests/ShowTile.test.js
@@ -1,22 +1,31 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 import ShowTile from '../ShowTile';
 
-describe('Must have properties', () => {
-  it('expects to render null when there are no props', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<ShowTile />);
-
-    const result = renderer.getRenderOutput();
-    const expected = null;
-    expect(result).toEqual(expected);
-  });
-});
+const data = {
+  "id" : "0",
+  "image_url" : "images/tile.jpg",
+  "image_alt" : "Steve Peacocke, Dan Ewing, Johnny Ruffo, Bonnie Sveen",
+  "show_name" : "Home and Away",
+  "link_url" : "//au.tv.yahoo.com/home-and-away/",
+  "logoAltText": "Channel 7",
+  "logoSrc": "images/logo.png"
+};
 
 it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<ShowTile />, div);
-  renderer.create(<ShowTile />);
+  shallow(<ShowTile linkUrl={data.link_url} showName={data.show_name} logoAltText={data.logoAltText} logoSource={data.logoSrc} displayLogo={true} imageSource={data.image_url} imageAltText={data.image_alt} isLinkExternal={true}/>);
+});
+
+it('Does not render the logo when displayLogo is false', () => {
+  const component = shallow(<ShowTile linkUrl={data.link_url} showName={data.show_name} logoAltText={data.logoAltText} logoSource={data.logoSrc} displayLogo={false} imageSource={data.image_url} imageAltText={data.image_alt} isLinkExternal={true}/>);
+  const result = component.find('.show-tile--logo').exists();
+
+  expect(result).toEqual(false);
+});
+
+it('Has a link with an aria-label to say it will open an external link when isLinkExternal is true', () => {
+  const component = shallow(<ShowTile linkUrl={data.link_url} showName={data.show_name} logoAltText={data.logoAltText} logoSource={data.logoSrc} displayLogo={false} imageSource={data.image_url} imageAltText={data.image_alt} isLinkExternal={true}/>);
+  const link = component.find('.show-tile a[aria-label]');
+  expect(link.exists()).toEqual(true);
+  expect(link.prop('aria-label')).toEqual('this link will open a new window')
 });
